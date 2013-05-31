9fef1975edf836e3c5594a55f3717f7256495e92
Webprofusion <development@webprofusion.com>
2013-05-31 20:51:13 +0800

    Move non-core Models into Extended folder

---
+++ API/OCM.Net/OCM.API.Model/Extended/EditQueueFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OCM.API.Common.Model
+{
+    public class EditQueueFilter
+    {
+        public bool ShowEditsOnly { get; set; }
+        public bool ShowProcessed { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+        public int MinimumDifferences { get; set; }
+        public int MaxResults { get; set; }
+
+        public EditQueueFilter()
+        {
+            MinimumDifferences = 1;
+            MaxResults = 200;
+        }
+    }
+}
