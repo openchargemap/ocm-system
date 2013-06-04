738d8d083d748b4590c622adb017ace5d7775ceb
Christopher Cook <christopher.cook@webprofusion.com>
2013-06-04 16:54:44 +0800

    Style tweaks, begin moving Widgets to main site

---
+++ Website/OCM.MVC/Controllers/WidgetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OCM.MVC.Controllers
+{
+    public class WidgetController : Controller
+    {
+        //
+        // GET: /Widget/
+
+        public ActionResult GeoChart()
+        {
+            return View();
+        }
+
+    }
+}
