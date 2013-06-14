4f51e98edecfae16b86d85a0ea09db5038a20474
webprofusion-chrisc <christopher.cook@webprofusion.com>
2013-06-14 17:07:47 +0800

    Add general exception logging

--- API/OCM.Net/OCM.API.Core/Common/AuditLogManager.cs
+++ API/OCM.Net/OCM.API.Core/Common/AuditLogManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using OCM.API.Common.Model;
+using System.Collections;
+using System.Configuration;
 
 namespace OCM.API.Common
 {
@@ -13,32 +15,105 @@ namespace OCM.API.Common
         PermissionRemoved = 30,
         CreatedItem = 100,
         UpdatedItem = 200,
-        DeletedItem = 300
+        DeletedItem = 300,
+        SystemErrorWeb = 1000,
+        SystemErrorAPI = 1010
     }
 
     public class AuditLogManager
     {
-        
+        public static void ReportWebException(HttpServerUtility Server, AuditEventType eventType)
+        {
+
+            bool ignoreException = false;
+            string body = "An error has occurred while a user was browsing OCM:<br><br>";
+
+            object exceptionObject = null;
+            if (Server.GetLastError() != null)
+            {
+                Exception exp = Server.GetLastError();
+                exceptionObject = exp;
+
+                if (exp.InnerException != null)
+                {
+                    exceptionObject = exp.InnerException;
+                }
+
+                body += ((Exception)exceptionObject).ToString();
+
+                HttpContext con = HttpContext.Current;
+                if (con.Request.Url != null)
+                {
+                    body += "<br><br>Request Url:" + con.Request.Url.ToString();
+
+                    //special case to avoid reporting /trackback url exceptions
+                    if (con.Request.Url.ToString().EndsWith("/trackback/")) ignoreException = true;
+
+                }
+                body += "<br><br>" + DateTime.UtcNow.ToString();
+            }
+
+            if (exceptionObject is System.Web.HttpRequestValidationException || exceptionObject is System.Web.UI.ViewStateException) ignoreException = true;
+
+            if (!ignoreException)
+            {
+                if (ConfigurationManager.AppSettings["EnableErrorNotifications"] == "true")
+                {
+                    NotificationManager notification = new NotificationManager();
+
+                    var msgParams = new Hashtable(){
+                    {"Description", "System Error"},
+                    {"Name", "OCM Website"},
+                    {"Email", "system@openchargemap.org"},
+                    {"Comment", body}
+                };
+
+                    notification.PrepareNotification(NotificationType.ContactUsMessage, msgParams);
+                    notification.SendNotification(NotificationType.ContactUsMessage);
+
+                }
+
+                AuditLogManager.Log(null, eventType, body, null);
+
+            }
+        }
+
         public static void Log(User user, AuditEventType eventType, string eventDescription, string comment)
         {
-            OCM.Core.Data.OCMEntities dataModel = new OCM.Core.Data.OCMEntities();
-            var auditEntry = new Core.Data.AuditLog();
-            auditEntry.UserID = user.ID;
-            auditEntry.EventDescription = "["+eventType.ToString()+"]:" +(eventDescription!=null?eventDescription:"");
-            auditEntry.Comment = comment;
-            auditEntry.EventDate = DateTime.UtcNow;
-            
-            dataModel.AuditLogs.Add(auditEntry);
-            dataModel.SaveChanges();
+            try
+            {
+                OCM.Core.Data.OCMEntities dataModel = new OCM.Core.Data.OCMEntities();
+                var auditEntry = new Core.Data.AuditLog();
+
+                if (user == null)
+                {
+                    auditEntry.User = dataModel.Users.First(u => u.ID == (int)StandardUsers.System);
+                }
+                else
+                {
+                    auditEntry.UserID = user.ID;
+                }
+
+                auditEntry.EventDescription = "[" + eventType.ToString() + "]:" + (eventDescription != null ? eventDescription : "");
+                auditEntry.Comment = comment;
+                auditEntry.EventDate = DateTime.UtcNow;
+
+                dataModel.AuditLogs.Add(auditEntry);
+                dataModel.SaveChanges();
+            }
+            catch (Exception)
+            {
+                //TODO: fallback to alternative logging
+            }
         }
 
         public List<AuditLog> Get(int? userID, DateTime? fromDate)
         {
             OCM.Core.Data.OCMEntities dataModel = new Core.Data.OCMEntities();
-            var list = dataModel.AuditLogs.Where(a=>
-                    (fromDate==null || (fromDate!=null && a.EventDate>=fromDate))
-                    && (userID==null || (userID!=null && a.UserID==userID))
-                ).OrderByDescending(a=>a.EventDate);
+            var list = dataModel.AuditLogs.Where(a =>
+                    (fromDate == null || (fromDate != null && a.EventDate >= fromDate))
+                    && (userID == null || (userID != null && a.UserID == userID))
+                ).OrderByDescending(a => a.EventDate);
 
             List<AuditLog> auditLog = new List<AuditLog>();
             foreach (var log in list)
--- API/OCM.Net/OCM.API.Web/Global.asax.cs
+++ API/OCM.Net/OCM.API.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OCM.API.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -35,6 +36,22 @@ namespace OCM.API
             //http://stackoverflow.com/questions/2416182/garbled-error-page-output-using-gzip-in-asp-net-iis7
             HttpApplication app = sender as HttpApplication;
             app.Response.Filter = null;
+
+
+            // Get the exception object.
+            Exception exc = Server.GetLastError();
+
+            // Handle HTTP errors
+            if (exc.GetType() == typeof(HttpException))
+            {
+                if (exc.Message.Contains("NoCatch") || exc.Message.Contains("maxUrlLength")) return;
+            }
+
+            AuditLogManager.ReportWebException(Server, AuditEventType.SystemErrorAPI);
+
+            Server.ClearError();
+            Response.StatusCode = 500;
+            Response.End();
         }
 
         protected void Session_End(object sender, EventArgs e)
