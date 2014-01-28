2cb5eea477ec15334f71321b51c48f962e7f4482
Christopher Cook <christopher.cook@webprofusion.com>
2014-01-28 13:57:43 +0800

    Add flag to indicate which status stypes are user selectable

--- API/OCM.Net/OCM.API.Model/Base/StatusType.cs
+++ API/OCM.Net/OCM.API.Model/Base/StatusType.cs
@@ -7,5 +7,6 @@ namespace OCM.API.Common.Model
     public class StatusType : SimpleReferenceDataType
     {
         public bool? IsOperational { get; set; }
+        public bool IsUserSelectable { get; set; }
     }
 }
