d2ad8778d9d93869240cd3b8cb90b2351298ce38
webprofusion-chrisc <christopher.cook@webprofusion.com>
2015-03-27 12:02:11 +0800

    Begin adding xUnit based tests for API/Core

---
+++ Tests/OCM.API.Tests/TestPOIQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OCM.API.Tests
+{
+    /// <summary>
+    /// Range of tests on filter parameters for POI retrieval
+    /// </summary>
+    public class TestPOIQuery
+    {
+        [Fact]
+        public void FilterOnChargePointID()
+        {
+            var api = new OCM.API.Common.POIManager();
+            var results = api.GetChargePoints(new Common.APIRequestParams { ChargePointIDs = new int[] { 10000 } });
+            Assert.Equal(results.Count, 1);
+
+            var poi = results.FirstOrDefault();
+            Assert.NotNull(poi);
+
+            Assert.Equal(poi.AddressInfo.CountryID, 168); //Norway
+        }
+    }
+}
