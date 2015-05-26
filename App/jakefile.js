3e31113ab7b3c8fcb2299e720406fecacdee6601
webprofusion-chrisc <christopher.cook@webprofusion.com>
2015-05-26 20:37:45 +0800

    App: version bump

--- App/jakefile.js
+++ App/jakefile.js
@@ -12,7 +12,7 @@ var util = require("util");
 
 /* Globals */
 var buildDate = new Date();
-var releaseVersion = "5.2.0_" + moment().format('YYYYMMDD');
+var releaseVersion = "5.2.1_" + moment().format('YYYYMMDD');
 var indexFileName = "index.html";
 var srcDir = "./";
 var buildDir = "build/output";
