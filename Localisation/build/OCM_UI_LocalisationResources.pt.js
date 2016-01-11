079f2a26461621f653699be338b179bd0f31cf64
webprofusion-chrisc <christopher.cook@webprofusion.com>
2016-01-11 15:58:04 +0800

    translation updates

--- Localisation/build/OCM_UI_LocalisationResources.pt.js
+++ Localisation/build/OCM_UI_LocalisationResources.pt.js
@@ -3,38 +3,38 @@ var localisation_dictionary_pt ={
   "_langCode": "pt",
   "ocm": {
     "general": {
-      "shortDescription": null,
-      "linkWebApp": null,
-      "linkBrowsePOI": null,
-      "browsePOI": null,
-      "loadingProgress": null,
-      "addLocation": null,
-      "addItem": null,
-      "favourites": null,
-      "settings": null,
-      "about": null,
-      "signIn": null,
-      "signOut": null
+      "shortDescription": "O Ultimo registo global inserido sobre a localização dos pontos de carga.",
+      "linkWebApp": "A procurar localizações usando a nossa Web App",
+      "linkBrowsePOI": "Explorar todos os locais",
+      "browsePOI": "Explorar Localizações de carregamento",
+      "loadingProgress": "Carregando...",
+      "addLocation": "Adicionar local",
+      "addItem": "Adicionar",
+      "favourites": "Favoritos",
+      "settings": "Opções",
+      "about": "Sobre",
+      "signIn": "Iniciar sessão",
+      "signOut": "Terminar Sessão"
     },
     "details": {
       "sectionTitle": "Localização de carregamento",
       "location": {
         "sectionTitle": "Detalhes do local",
-        "title": null,
+        "title": "Título de localização",
         "address": "Endereço mais próximo",
-        "addressLine1": null,
-        "addressLine2": null,
-        "town": null,
-        "stateOrProvince": null,
+        "addressLine1": "Linha de morada 1",
+        "addressLine2": "Linha de morada 2",
+        "town": "Vila/Cidade",
+        "stateOrProvince": "Estado/Província",
         "postcode": "Código postal",
         "country": "País",
-        "latitude": null,
-        "longitude": null,
-        "contactTelephone1": null,
-        "contactTelephone2": null,
+        "latitude": "Latitude",
+        "longitude": "Longitude",
+        "contactTelephone1": "Telefone de contato principal",
+        "contactTelephone2": "Telefone de contato secundário",
         "contactEmail": "Contato e-mail",
         "map": "mapa",
-        "streetView": null,
+        "streetView": "Vista de rua",
         "addressRelatedURL": "Site da Web"
       },
       "generalComments": "Informações gerais",
@@ -42,15 +42,15 @@ var localisation_dictionary_pt ={
       "advancedDetails": "Informações adicionais",
       "numberOfPoints": "Número de estações/lugares",
       "usageType": "Uso",
-      "usageCost": null,
+      "usageCost": "Custo de uso",
       "operationalStatus": "Estado Operacional",
-      "datePlanned": null,
-      "dateLastConfirmed": null,
+      "datePlanned": "Data planeada",
+      "dateLastConfirmed": "Ultima data confirmada",
       "lastUpdated": "Ultima atualização",
-      "refNumber": null,
-      "dataQualityLevel": null,
-      "importedDataNotice": null,
-      "submissionStatus": null,
+      "refNumber": "Ref OpenChargeMap",
+      "dataQualityLevel": "Qualidade da informação prestada",
+      "importedDataNotice": "Estes dados são gerados automaticamente, as modificações devem ser submetidas para a administração para aprovação.",
+      "submissionStatus": "Estado de submissão",
       "placesNearby": {
         "sectionTitle": "Locais perto"
       },
@@ -64,28 +64,28 @@ var localisation_dictionary_pt ={
       "commentsAndRatings": {
         "sectionTitle": "Comentários e avaliações",
         "zeroComments": "Nenhum comentário do utilizador enviado",
-        "addPrompt": null
+        "addPrompt": "Adicione um comentário ou coloque o estado do mesmo"
       },
       "equipment": {
         "sectionTitle": "Detalhes do equipamento",
         "connectionType": "Tipo de ligação",
-        "powerKW": null,
-        "powerLevel": null,
+        "powerKW": "Potência máxima (KW)",
+        "powerLevel": "Nível",
         "quantity": "Quantidade",
-        "supplyType": null,
+        "supplyType": "Tipo de alimentação",
         "voltage": "Voltagem",
-        "current": null,
-        "comment": null,
-        "reference": null
+        "current": "Corrente máxima",
+        "comment": "Comentários",
+        "reference": "Referência"
       },
       "dataProvider": {
-        "sectionTitle": null,
+        "sectionTitle": "Provedor de dados",
         "website": "Site",
-        "providersReference": null
+        "providersReference": "Ref de provedores"
       },
       "operator": {
-        "sectionTitle": null,
-        "operatorsReference": null
+        "sectionTitle": "Rede/Operador",
+        "operatorsReference": "Ref de operadores"
       }
     },
     "search": {
@@ -94,7 +94,7 @@ var localisation_dictionary_pt ={
       "useMyLocation": "Pesquisar perto de mim",
       "distance": "Distância",
       "chargingLevels": "Nível de carga",
-      "minPowerKW": null,
+      "minPowerKW": "Minima Potência em Kw",
       "connectionTypes": "Tipos de conexão",
       "networkOperators": "Rede/Operador",
       "countries": "País",
@@ -105,126 +105,154 @@ var localisation_dictionary_pt ={
       "progress": "Procurando.."
     },
     "navigation": {
-      "home": null,
+      "home": "Início",
       "browse": {
-        "sectionTitle": null,
-        "allLocations": null,
-        "byCountry": null,
-        "avaitingReview": null,
-        "recentActivity": null,
-        "webApp": null,
-        "statistics": null
+        "sectionTitle": "Explorar",
+        "allLocations": "Todos os Locais",
+        "byCountry": "Por País",
+        "avaitingReview": "Aguardando revisão",
+        "recentActivity": "Atividade Recente",
+        "webApp": "Web App para telemóvel",
+        "statistics": "Estatísticas",
+        "awaitingReviewImported": "Localizações importadas a aguardarem aprovação",
+        "editqueue": "Edição a aguardar aprovação"
       },
       "develop": {
-        "sectionTitle": null,
-        "api": null,
-        "htmlWidgets": null,
-        "examples": null,
-        "getInvolved": null,
-        "code": null,
-        "translate": null
+        "sectionTitle": "Desenvolver",
+        "api": "API",
+        "htmlWidgets": "Widgets HTML",
+        "examples": "Exemplos",
+        "getInvolved": "Envolver-se",
+        "code": "Verifique o código (on GitHub)",
+        "translate": "Ajude na tradução"
       },
-      "discuss": null,
+      "discuss": "Discutir",
       "about": {
-        "termsPrivacy": null,
-        "contact": null,
-        "guidance": null
+        "termsPrivacy": "Termos & privacidade",
+        "contact": "Contato",
+        "guidance": "Guia dos Contribuintes"
       },
       "profile": {
-        "sectionTitle": null,
-        "subscriptions": null,
-        "comments": null,
-        "uploads": null
+        "sectionTitle": "Meu perfil",
+        "subscriptions": "As Minhas Subscrições",
+        "comments": "Meus Comentários",
+        "uploads": "Meus uploads"
       }
     },
     "infoText": {
       "getInvolved": {
-        "youCan": null,
-        "browseLocations": null,
-        "commentsPhotos": null,
-        "dontSeePOI": null
+        "youCan": "Você pode",
+        "browseLocations": "A explorar localizações perto de si",
+        "commentsPhotos": "Adicione os seus comentários e fotos",
+        "dontSeePOI": "Não consegue encontrar um ponto de carga mas que sabe que existe?"
       },
       "getStations": {
-        "sectionTitle": null,
-        "who": null,
-        "what": null,
-        "websiteOperators": null,
-        "embedMap": null
+        "sectionTitle": "Informações sobre o ponto de Carga",
+        "who": "Informações dos vários Fabricantes, Programadores da App, site oficial",
+        "what": "Consegue acesso e baixar todos os últimos pontos de carga usando a nossa",
+        "websiteOperators": "Se tem um site e pretende incluir a localização do nosso site, pode",
+        "embedMap": "Inserir o mapa no seu site"
       },
       "supplyData": {
-        "sectionTitle": null,
-        "who": null,
-        "needData": null,
-        "what": null,
-        "contactUs": null,
-        "getStarted": null
+        "sectionTitle": "Programadores e provedores de informação",
+        "who": "EVSE fornecedores, Operadores de rede, Registos Nacionais, Carregamentos locais informações apps & sites:",
+        "needData": "Nós precisamos da sua informação",
+        "what": "Pode nos fornecer com a sua ultima visita num ponto de carga, localização e estado, independentemente sobre a sua profissão ou empresa, fácil de funcionar, disponibiliza toda a informação e localização dos pontos de carga.",
+        "contactUs": "Contacte nos",
+        "getStarted": "Para começar"
       },
       "stats": {
-        "locations": null,
+        "locations": "Localizações",
         "locationSummary": null
       },
-      "shortProjectSummary": null,
-      "getApp": null,
-      "tip": null,
+      "shortProjectSummary": "Open Charge Map é uma organização não comercial alojada e suportada por uma comunidade de empresas, programadores, utilizadores e todos os interessados em todo o mundo.",
+      "getApp": "Baixar a App",
+      "tip": "Dica",
       "searching": {
-        "quickWay": null,
-        "webApp": null,
-        "searchAppStore": null
+        "quickWay": "Se deseja uma maneira rápida de chegar ao ponto de carga  mais proximo de si, pode tentar o nosso",
+        "webApp": "App via internet",
+        "searchAppStore": "especialmente nos equipamentos móveis. Pode também procurar no seu equipamento a APP Store por uma versão em que pode carregar."
       },
+      "sponsors": "Estamos a procurar patrocinadores e parceiros, acha que consegue ajudar?",
+      "termsNotice": null,
       "about": {
+        "ourService": "O Nosso Serviço",
+        "aim": {
+          "title": "O Nosso Objectivo",
+          "p1": null,
+          "p2": null
+        },
+        "whyOCM": {
+          "title": "Porquê usar o Open Charge Map?",
+          "content": null
+        },
         "funding": {
-          "title": null,
+          "title": "A Fundar",
           "p1": null
+        },
+        "developersContributors": {
+          "sectionTitle": "Programadores e colaboradores",
+          "p1": null,
+          "p2": null
+        },
+        "projectStats": {
+          "sectionTitle": null,
+          "general": null
+        },
+        "dataSources": {
+          "title": "Informações dos dados",
+          "p1": null,
+          "p2": null
         }
       }
     },
     "reference": {
       "statusType": {
-        "value_minus1": null,
-        "value_0": null,
-        "value_10": null,
-        "value_20": null,
-        "value_30": null,
-        "value_50": null,
-        "value_75": null,
-        "value_100": null,
-        "value_150": null,
-        "value_200": null
+        "value_minus1": "Nenhum seleccionado",
+        "value_0": "Desconhecido",
+        "value_10": "Disponível de momento; estado automatico",
+        "value_20": "Em uso, estado automatico",
+        "value_30": "Temporariamente indisponível",
+        "value_50": "A Funcionar",
+        "value_75": "Parcialmente a funcionar",
+        "value_100": "Avariado",
+        "value_150": "Planeado Futuramente",
+        "value_200": "Removido"
       },
       "operatorInfo": {
-        "value_minus1": null,
-        "value_1": null,
-        "value_44": null,
-        "value_45": null
+        "value_minus1": "Nenhum selecionado",
+        "value_1": "Operador desconhecido",
+        "value_44": "Habitação pessoal",
+        "value_45": "Empresa proprietária na Localização"
       },
       "usageType": {
-        "value_minus1": null,
-        "value_0": null,
-        "value_1": null,
-        "value_2": null,
-        "value_3": null,
-        "value_4": null,
-        "value_5": null,
-        "value_6": null,
-        "value_7": null
+        "value_minus1": "Nenhum selecionado",
+        "value_0": "Desconhecido",
+        "value_1": "Publico",
+        "value_2": "Privado - Acesso restrito",
+        "value_3": "Privado a proprietário, acesso exclusivo",
+        "value_4": "Publico - Membro registados unicamente",
+        "value_5": "Publico - Pago no Local",
+        "value_6": "Privado - Para empregados e visitantes",
+        "value_7": "Publico - Necessário informar"
       },
       "connectionType": {
-        "value_minus1": null,
-        "value_0": null
+        "value_minus1": "nenhum selecionado",
+        "value_0": "Desconhecido"
       },
       "levelType": {
-        "value_minus1": null,
-        "value_0": null,
-        "value_1": null,
-        "value_2": null,
-        "value_3": null
+        "value_minus1": "nenhum selecionado",
+        "value_0": "(desconhecido)",
+        "value_1": "Nivel 1; Lento; abaixo de 2Kw",
+        "value_2": "Nível 2 Medio; acima 2Kw",
+        "value_3": "Nivel 3 Alto Acima dos 40Kw"
       },
       "supplyType": {
-        "value_minus1": null,
-        "value_0": null,
-        "value_10": null,
-        "value_20": null,
-        "value_30": null
+        "value_minus1": "nenhum selecionado",
+        "value_0": "(desconhecido)",
+        "value_10": "AC (Fase-Unica)",
+        "value_20": "AC (Três-Fases)",
+        "value_30": "DC"
       }
     }
   }
