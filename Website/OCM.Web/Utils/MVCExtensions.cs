324f63f9aa56cc42b2a6838a4527fae8c146c26d
webprofusion-chrisc <christopher.cook@webprofusion.com>
2019-11-06 20:00:15 +0800

    Don't include port in URI

--- Website/OCM.Web/Utils/MVCExtensions.cs
+++ Website/OCM.Web/Utils/MVCExtensions.cs
@@ -14,11 +14,11 @@ namespace OCM.MVC
     {
         public static Uri GetUri(this HttpRequest request)
         {
+
             var uriBuilder = new UriBuilder
             {
                 Scheme = request.Scheme,
                 Host = request.Host.Host,
-                Port = request.Host.Port.GetValueOrDefault(80),
                 Path = request.Path.ToString(),
                 Query = request.QueryString.ToString()
             };
