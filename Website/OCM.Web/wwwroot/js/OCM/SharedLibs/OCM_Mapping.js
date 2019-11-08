ccb5f26d239b7736c65bc163ce92f5dbf136ec40
webprofusion-chrisc <christopher.cook@webprofusion.com>
2019-11-08 14:30:54 +0800

    Update js builds

--- Website/OCM.Web/wwwroot/js/OCM/SharedLibs/OCM_Mapping.js
+++ Website/OCM.Web/wwwroot/js/OCM/SharedLibs/OCM_Mapping.js
@@ -1,12 +1,9 @@
 /// <reference path="OCM_Base.ts" />
 /// <reference path="OCM_CommonUI.ts" />
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
