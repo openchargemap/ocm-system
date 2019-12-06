0c898ca1d120dd232ca9037c2a056b3388e476fc
webprofusion-chrisc <christopher.cook@webprofusion.com>
2019-12-06 14:17:46 +0800

    Implement settings as json

---
+++ API/OCM.Net/OCM.API.Core/Util/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCM.Core.Settings
+{
+    public class CoreSettings
+    {
+        public string CachePath { get; set; }
+        public bool EnableDataWrites { get; set; }
+        public ApiKeys ApiKeys { get; set; }
+        public MongoDbSettings MongoDBSettings { get; set; }
+    }
+
+    public class ApiKeys
+    {
+        public string MapQuestOpenAPIKey { get; set; }
+
+        public string OSMApiKey { get; set; }
+    }
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+        public int MaxCacheAgeMinutes { get; set; }
+    }
+}
