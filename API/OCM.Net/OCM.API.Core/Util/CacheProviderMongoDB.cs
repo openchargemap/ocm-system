openchargemap/ocm-system

commit bc61dca9ec400ecabf049462429f791eff447530

Adjust bounding box indexing and ensure indexes

API/OCM.Net/OCM.API.Core/Util/CacheProviderMongoDB.cs
@@ -160,6 +160,8 @@ namespace OCM.Core.Data
                             if (_settings == null) throw new Exception("Cache Provider Default Instance requires init using CreateDefaultInstance");
 
                             _instance = new CacheProviderMongoDB();
+
+                            _instance.EnsureMongoDBIndexes();
                         }
                     }
                 }
@@ -421,7 +423,7 @@ namespace OCM.Core.Data
         /// <returns></returns>
         protected void EnsureMongoDBIndexes() {
             var poiCollection = database.GetCollection<POIMongoDB>("poi");
-            poiCollection.CreateIndex(IndexKeys.GeoSpatial("SpatialPosition.coordinates")); // bounding box queries
+            poiCollection.CreateIndex(IndexKeys.GeoSpatialSpherical("SpatialPosition.coordinates")); // bounding box queries
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.GeoSpatialSpherical(x => x.SpatialPosition)); // distance queries
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.Descending(x => x.DateLastStatusUpdate));
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.Descending(x => x.DateCreated));

commit 0e05dcefee2c542337aff2a388b5691fa5986ae5

Create a 2d index for "SpatialPosition.coordinates"
This fixes a regression introduced by bc61dca9ec400ecabf049462429f791eff447530.

See https://github.com/openchargemap/ocm-system/issues/170 and related

API/OCM.Net/OCM.API.Core/Util/CacheProviderMongoDB.cs
@@ -423,7 +423,7 @@ namespace OCM.Core.Data
         protected void EnsureMongoDBIndexes()
         {
             var poiCollection = database.GetCollection<POIMongoDB>("poi");
-            poiCollection.CreateIndex(IndexKeys.GeoSpatialSpherical("SpatialPosition.coordinates")); // bounding box queries
+            poiCollection.CreateIndex(IndexKeys.GeoSpatial("SpatialPosition.coordinates")); // bounding box queries
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.GeoSpatialSpherical(x => x.SpatialPosition)); // distance queries
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.Descending(x => x.DateLastStatusUpdate));
             poiCollection.CreateIndex(IndexKeys<POIMongoDB>.Descending(x => x.DateCreated));