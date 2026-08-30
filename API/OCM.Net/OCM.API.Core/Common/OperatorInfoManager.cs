759dcfbee3918d9404c874468bd8002113302a6a
Juan Marco <jmmarco@gmail.com>
2026-08-30 09:56:33 +0200

    fix(operator-proposals): keep direct save validation with management UI

--- API/OCM.Net/OCM.API.Core/Common/OperatorInfoManager.cs
+++ API/OCM.Net/OCM.API.Core/Common/OperatorInfoManager.cs
@@ -270,7 +270,7 @@ namespace OCM.API.Common
             return Model.Extensions.OperatorInfo.FromDataModel(operatorInfo);
         }
 
-        public OperatorInfo UpdateOperatorInfo(int userId, OperatorInfo update, bool confirmNotDuplicate = false)
+        public OperatorInfo UpdateOperatorInfo(int userId, OperatorInfo update)
         {
             var operatorInfo = new OCM.Core.Data.Operator();
             bool isUpdate = false;
@@ -280,16 +280,6 @@ namespace OCM.API.Common
                 operatorInfo = DataModel.Operators.FirstOrDefault(o => o.Id == update.ID);
                 isUpdate = true;
             }
-
-            var matches = FindPotentialDuplicates(update.Title, GetCountryCodeFromTitle(update.Title), update.WebsiteURL,
-                update.ContactEmail, isUpdate ? update.ID : (int?)null);
-            var duplicateTitle = matches.FirstOrDefault(match => match.MatchType == OperatorMatchType.DuplicateTitle);
-            if (duplicateTitle != null)
-                throw new InvalidOperationException($"\"{duplicateTitle.Operator.Title}\" already exists. Edit that operator instead of creating a duplicate.");
-
-            var needsConfirmation = matches.Where(match => match.RequiresConfirmation).ToList();
-            if (needsConfirmation.Any() && !confirmNotDuplicate)
-                throw new InvalidOperationException($"This may be a duplicate of {string.Join(", ", needsConfirmation.Select(match => match.Operator.Title))}. Confirm this is a separate operator to save it.");
 
             operatorInfo.Title = update.Title;
             operatorInfo.WebsiteUrl = update.WebsiteURL;
